87616e0e4a2b76d9ea7d95b8fc145d496d3b4fda
bryan james libante <bryanlibante09@gmail.com>
2024-07-27 23:00:35 +0800

    student IU done

--- src/app/view_clearance/page.tsx
+++ src/app/view_clearance/page.tsx
@@ -1,9 +1,14 @@
+import StudentClearanceView from '@/components/student_clearance/student_clearance/student_clearance'
+import StudentInformation from '@/components/student_clearance/student_information/student_information'
 import React from 'react'
 
 const SubmitCredentials = () => {
   return (
-    <div>
-      WELCOME
+    <div className=' text-black bg-slate-200 p-10'>
+      <div className='flex justify-center text-black bg-slate-200'>
+            <StudentInformation/>
+      </div>
+      <StudentClearanceView/>
     </div>
   )
 }
