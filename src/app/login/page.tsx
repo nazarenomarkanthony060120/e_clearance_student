da459897b6ad5143f6274e84bc219193f462c69a
nazarenomarkanthony060120 <nazarenomarkanthony060120@gmail.com>
2024-09-23 18:45:58 +0800

    Categorize type of account

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,19 +1,22 @@
 import LoginForm from "@/components/form/login"
 import { BiLogIn } from 'react-icons/bi'
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 
 const Login = () => {
   return (
-    <div className='min-h-screen flex items-center justify-center text-black bg-slate-200'>
-        <div className='min-h-screen flex items-center justify-center text-black bg-slate-200'>
-          <div className="w-[350px] h-fit rounded-md shadow-md bg-white flex flex-col gap-9 p-5 ">
-              <div className="flex justify-center items-center flex-col gap-1">
-                  <BiLogIn className='text-[34px] text-gray-600'/>
-                  <span className="text-2xl font-bold text-[#d6543a] ">STUDENT LOGIN</span>
-              </div>
-            <LoginForm />
+
+    <><div className='min-h-screen flex items-center justify-center text-black bg-slate-200'>
+      <div className='min-h-screen flex items-center justify-center text-black bg-slate-200'>
+        <div className="w-[350px] h-fit rounded-md shadow-md bg-white flex flex-col gap-9 p-5 ">
+          <div className="flex justify-center items-center flex-col gap-1">
+            <BiLogIn className='text-[34px] text-gray-600' />
+            <span className="text-2xl font-bold text-[#d6543a] ">STUDENT LOGIN</span>
           </div>
+          <LoginForm />
         </div>
       </div>
+    </div><ToastContainer limit={4} /></>
   )
 }
 
